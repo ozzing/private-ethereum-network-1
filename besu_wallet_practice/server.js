915f1498d5747170554c92be70428c5f6e8428ec
Hyunjae Kim <peterhyunjae@naver.com>
2022-05-30 03:10:09 +0900

    simple send fin

--- besu_wallet_practice/server.js
+++ besu_wallet_practice/server.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get('/', function(req,res){
+  res.sendFile(__dirname + "/index.html")
+})
+
+app.get('/deploy', function(req,res){
+  res.sendFile(__dirname + "/deploy.html")
+})
+
 
 var server = app.listen(3000, function(){
   console.log("server is working now")
-})
-
-app.get('/', function(req,res){
-  res.send('hello world')
 })
