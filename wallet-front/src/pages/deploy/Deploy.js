3af4f1d3e9c55bd2f3cf3f794f89e7afd22ff50a
Hyunjae Kim <peterhyunjae@naver.com>
2022-06-01 20:23:39 +0900

    route finished

---
+++ wallet-front/src/pages/deploy/Deploy.js
@@ -0,0 +1,8 @@
+const Deploy = () => {
+  return (
+    <main style={{ padding: "1rem 0" }}>
+      <h2>Deploy</h2>
+    </main>
+  );
+}
+export default Deploy
---
+++ wallet-front/src/pages/send/Send.js
@@ -0,0 +1,8 @@
+const Send = () => {
+  return (
+    <main style={{ padding: "1rem 0" }}>
+      <h2>Send</h2>
+    </main>
+  );
+}
+export default Send
