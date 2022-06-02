9ba22b27902f011b3dec4d5b9031a2e7c18a5b9b
Hyunjae Kim <peterhyunjae@naver.com>
2022-06-02 14:06:46 +0900

    [style] : header centered

--- wallet-front/src/components/main/AccountHeader.js
+++ wallet-front/src/components/main/AccountHeader.js
@@ -6,21 +6,41 @@ const AccountHeader = () => {
   return (
     <>
       <HeaderWrapper>
-        <span></span>
-        <span>Account2</span>
-        <MoreVertIcon />
+        {/* <span></span> */}
+        <AccountWrapper>
+          <span>Account2</span>
+        </AccountWrapper>
+        <IconWrapper>
+          <MoreVertIcon />
+        </IconWrapper>
       </HeaderWrapper>
       <HorizontalLine />
     </>
   );
 };
-
+const IconWrapper = styled.div`
+  align-self: center;
+  justify-self: end;
+`;
+const AccountWrapper = styled.div`
+  grid-column: 2 / span 1;
+  place-self: center stretch;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+`;
 const HeaderWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 30% minmax(30%, 1fr) 30%;
+  -webkit-column-gap: 5px;
+  -moz-column-gap: 5px;
+  column-gap: 5px;
+  padding: 0 8px;
+  border-bottom: 1px solid var(--color-border-muted);
   width: 100%;
   height: 64px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
 `;
 
 export default AccountHeader;
